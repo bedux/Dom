b514966b5c3fc2b871e74a88cf0c5184c1ae5171
Marco Bedulli <bedux@Marcos-MBP.station>
2017-01-03 22:06:24 +0100

    msgs

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var gpio = require("pi-gpio");
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
+ 
+	
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/On/:id', function(req, res, next) {
+ 
+	gpio.open(req.params.id, "output", function(err) {		// Open pin 16 for output 
+	    gpio.write(req.params.id, 1, function() {			// Set pin 16 high (1) 
+	        gpio.close(req.params.id);						// Close pin 16 
+	    });
+	});
+
+  res.render('index', { title: 'Express' });
+});
+router.get('/Off/:id', function(req, res, next) {
+ 
+	gpio.open(req.params.id, "output", function(err) {		// Open pin 16 for output 
+	    gpio.write(req.params.id, 0, function() {			// Set pin 16 high (1) 
+	        gpio.close(req.params.id);						// Close pin 16 
+	    });
+	});
+
   res.render('index', { title: 'Express' });
 });
 
