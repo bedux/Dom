2e59e0ed52f0c4ec8a080160817abf7003754e15
Marco Bedulli <bedux@Marcos-MacBook-Pro.local>
2017-01-14 10:17:28 +0100

    s

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,38 @@
 var express = require('express');
 var router = express.Router();
-var gpio = require("pi-gpio");
+//var gpio = require("pi-gpio");
 
 /* GET home page. */
-
-router.get('/', function(req, res, next) {
- 
-	
-  res.render('index', { title: 'Express' });
-});
-
-
-router.get('/On', function(req, res, next) {
-	gpio.open(req.query.id, "output", function(err) {		// Open pin 16 for output 
-	    console.log(err);
-	    gpio.write(req.query.id, 1, function() {			// Set pin 16 high (1) 
-	       // gpio.close(req.query.id);						// Close pin 16 
-	    });
-	});
-
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/Off', function(req, res, next) {
- 
-	gpio.open(req.query.id, "output", function(err) {
-		    console.log(err);
-		// Open pin 16 for output 
-	    gpio.write(req.query.id, 0, function() {			// Set pin 16 high (1) 
-	        //gpio.close(req.query.id);						// Close pin 16 
-	    });
-	});
-
-  res.render('index', { title: 'Express' });
-});
+//
+// router.get('/', function(req, res, next) {
+//
+//
+//   res.render('index', { title: 'Express' });
+// });
+//
+//
+// router.get('/On', function(req, res, next) {
+// 	gpio.open(req.query.id, "output", function(err) {		// Open pin 16 for output
+// 	    console.log(err);
+// 	    gpio.write(req.query.id, 1, function() {			// Set pin 16 high (1)
+// 	       // gpio.close(req.query.id);						// Close pin 16
+// 	    });
+// 	});
+//
+//   res.render('index', { title: 'Express' });
+// });
+//
+// router.get('/Off', function(req, res, next) {
+//
+// 	gpio.open(req.query.id, "output", function(err) {
+// 		    console.log(err);
+// 		// Open pin 16 for output
+// 	    gpio.write(req.query.id, 0, function() {			// Set pin 16 high (1)
+// 	        //gpio.close(req.query.id);						// Close pin 16
+// 	    });
+// 	});
+//
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var request = require('request').defaults({ encoding: null});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+
+
+http.listen(3001,function () {
+  console.log("Web socket are listen on 3001");
+})
+
+
+function getImage() {
+  request.get("http://192.168.0.102:3001/image/jpeg.cgi", function (err, res, body) {
+    io.emit('newImage', new Buffer(body).toString('base64'));
+    console.log("newMsg");
+    getImage();
+
+  });
+}
+
+getImage();
+
 
 module.exports = router;
